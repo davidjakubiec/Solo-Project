738c0eab8fa94eeaa5a321ad6721f2102d785824
DavidJakubiec <dwjakubiec@gmail.com>
2022-07-26 13:32:28 -0700

    webpack installed

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function App() {
+    return (
+        <div>
+            <h1>thank you Chase and Jeff</h1>
+        </div>
+    )
+}
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '/dist'), 
+        filename: 'index.bundle.js'
+    }, 
+    devServer: {
+        static: path.resolve(__dirname, 'src'),
+        port: 3010,
+        open: true,
+        hot: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                 }
+                },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    }, 
+    plugins: [
+                new HtmlWebpackPlugin({
+                title: 'Development',
+                //absolute path in future
+                 template: './src/index.html'
+                })
+            ]
+};
